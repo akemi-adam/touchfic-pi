bb8b9466f67848c237e1d21c82ef37e364a24011
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2022-08-13 07:41:23 -0300

    adicionando tabela de gêneros ao db

---
+++ database/migrations/2022_08_13_103355_create_tb_genders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_genders', function (Blueprint $table) {
+            $table->id('gen_id');
+            $table->string('gen_gender', 50);
+            $table->integer('gen_visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_genders');
+    }
+};
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title')
+@section('title', 'Postagens')
     
 @section('content')
     <div class="container-posts">
