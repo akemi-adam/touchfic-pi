03575dc0094daa27db4224aa30c5f6529c263d82
Maria Luíza <102473250+marimalu@users.noreply.github.com>
2022-09-16 16:35:23 -0300

    estilo menu lateral e ícones

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,24 +4,24 @@
   			
     <h3>Touchfic</h3>
         <ul>					
-            <li><a href="{{route('root.home')}}">Página inicial</a></li>
-            <li><a href="{{route('user.show', Auth::user()->id)}}">Meu perfil</a></li>
-            <li><a href="{{route('post.index')}}">Linha do tempo</a></li>
+            <li><a href="{{route('root.home')}}"><i class="fa-solid fa-hashtag"></i> Página inicial</a></li>
+            <li><a href="{{route('user.show', Auth::user()->id)}}"><i class="fa-solid fa-circle-user"></i> Meu perfil</a></li>
+            <li><a href="{{route('post.index')}}"><i class="fa-solid fa-bars-staggered"></i> Linha do tempo</a></li>
         </ul>
 
     <hr>
         <ul>
-            <li><a href="{{route('storie.create')}}">Criar uma história</a></li>
-            <li><a href="{{route('storie.mystories', Auth::user()->id)}}">Minhas histórias</a></li>
-            <li><a href="{{route('storie.likes', Auth::user()->id)}}">Histórias favoritas</a></li>
+            <li><a href="{{route('storie.create')}}"><i class="fa-solid fa-circle-plus"></i> Criar uma história</a></li>
+            <li><a href="{{route('storie.mystories', Auth::user()->id)}}"><i class="fa-solid fa-book-open-reader"></i> Minhas histórias</a></li>
+            <li><a href="{{route('storie.likes', Auth::user()->id)}}"><i class="fa-solid fa-heart"></i> Histórias favoritas</a></li>
         </ul>
 
     <hr>
         <ul>					
-            <li><a href="#">Tema</a></li>
+            <li><a href="#"><i class="fa-solid fa-circle-half-stroke"></i> Tema</a></li>
             @if (Auth::check())
             @if (Auth::user()->permission_id === 3)
-            <li><a href="{{route('admin.dashboard')}}">Opções de administrador</a></li>
+            <li><a href="{{route('admin.dashboard')}}"><i class="fa-solid fa-gear"></i> Opções de administrador</a></li>
             @endif
             @endif			
         </ul>
