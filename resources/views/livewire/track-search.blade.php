11f1c682b52233c67cb0c3c9272b7f1e72a68c03
Maria Luíza <102473250+marimalu@users.noreply.github.com>
2022-09-29 11:30:05 -0300

    estilo de formulário de criar capítulo

--- resources/views/livewire/track-search.blade.php
+++ resources/views/livewire/track-search.blade.php
@@ -1,8 +1,12 @@
-<div>
-    <input type="text" wire:model="search" name="search">
-    <button wire:click="$emit('listTracks')" type="button">
-        Procurar
-    </button>
+<div class="spotify-search">
+    <p>Selecionar música do <span><i class="fa-brands fa-spotify"></i> Spotify</span> para capítulo (opcional, somente prévia)</p>
+    <input type="text" wire:model="search" name="search" placeholder="Pesquise no Spotify...">
+
+    <div class="button-container">
+        <button wire:click="$emit('listTracks')" type="button">
+            Procurar
+        </button>
+    </div>
     @if ($listed)
     <select name="track" size="10">
         @foreach ($tracks['tracks']['items'] as $track)
--- resources/views/storie/chapter/create.blade.php
+++ resources/views/storie/chapter/create.blade.php
@@ -3,10 +3,11 @@
 @section('title', "$storie->title: Novo capítulo")
 
 @section('content')
-    <div class="container-create">
-        <h1>
+<main class="storie-form-container">
+    <section class="storie-form">
+        <h2>
             Novo capítulo
-        </h1>
+        </h2>
         <div>
 
             {{-- Error messages --}}
@@ -24,9 +25,11 @@
                 @csrf
                 <label for="title" class="label-tag">Título</label>
                 <input type="text" name="title" class="input-title" placeholder="Título do capítulo">
-                <label for="authornotes" class="label-tag">Notas do autor</label><br>
-                <textarea name="authornotes" cols="90" rows="15" class="desc-textearea" placeholder="Algo a dizer sobre o capítulo? Escreva aqui!"></textarea>
-                <label for="content">Capítulo</label><br>
+
+                <label for="authornotes" class="label-tag">Notas do autor (opcional)</label>
+                <textarea name="authornotes" cols="90" rows="10" class="desc-textearea" placeholder="Tem algo a dizer sobre o capítulo? Escreva aqui!"></textarea>
+
+                <label for="content">Capítulo</label>
                 <textarea name="content" cols="90" rows="15" class="desc-textearea" placeholder="Ponha a sua imaginação aqui..."></textarea>
 
                 @livewire('track-search')
@@ -34,11 +37,12 @@
                 <div class="container-submit">
                     <div class="submit-story-button">
                         <button>
-                            Enviar
+                            Enviar capítulo
                         </button>
                     </div>
                 </div>
             </form>
         </div>
-    </div>
+    </section>
+</main>
 @endsection
