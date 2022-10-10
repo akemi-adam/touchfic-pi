ce59c01385166f87dd7a64e8e0e706890b2a44ad
Maria Luíza <102473250+marimalu@users.noreply.github.com>
2022-10-10 18:50:05 -0300

    estilo da pág show de storie e botão curtir

--- resources/views/livewire/like.blade.php
+++ resources/views/livewire/like.blade.php
@@ -1,11 +1,11 @@
 <div>
     @if (!$this->status())
         <form wire:submit.prevent="enjoy()">
-            <button id="enjoy" class="like-button" onclick="incrementDisplay()">Curtir</button>
+            <button id="enjoy" class="like-button" onclick="incrementDisplay()"><i class="fa-regular fa-heart"></i> Curtir</button>
         </form>
     @else
         <form wire:submit.prevent="unlike()">
-            <button id="unlike" class="like-button" onclick="decrementDisplay()">Descurtir</button>
+            <button id="unlike" class="like-button" onclick="decrementDisplay()"><i class="fa-solid fa-heart"></i> Descurtir</button>
         </form>
     @endif
 </div>
