98261edc0f0d74f29645447175db31d8795cdabd
Maria Luíza <102473250+marimalu@users.noreply.github.com>
2022-11-07 17:50:06 -0300

    script para fechar sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById('smenu')
 
     sidebar.addEventListener('click', () => {
-        document.getElementById('sidebar').style.transform = "translateX(350px)"
+        document.getElementById('sidebar').style.transform = "translateX(330px)"
+    })
+
+    sidebar.addEventListener('mouseleave', () => {
+        document.getElementById('sidebar').style.transform = "translateX(-330px)"
     })
 
 })
