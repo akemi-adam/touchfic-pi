a70b7c442bfcec486772021d1a49c4967f6b4abf
Maria Luíza <102473250+marimalu@users.noreply.github.com>
2022-11-08 14:12:00 -0300

    estilo da página da linha do tempo

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,51 +1,61 @@
 @extends('layouts.main')
 
-@section('title', 'Postagens')
+@section('title', 'Postagens - Touchfic')
     
 @section('content')
+
 @can('authenticated')
     <a href="{{ route('post.create') }}" class="post-button">
        <i class="fa-solid fa-plus"></i>
     </a>
 @endcan
+
     <h2 class="title">
         Postagens
     </h2>
     
         <div class="container-posts">
 
-        <div class="div-posts">
+        
             @forelse ($posts as $post)
-                <img src="{{ FileSupport::getAvatar($post->user->avatar) }}" alt="" class="avatar">
+            <div class="div-posts">
+                <div class="username">
+                    <img src="{{ FileSupport::getAvatar($post->user->avatar) }}" alt="" class="avatar">
                 <p>
                     <strong>
-                        {{ $post->user->name }} disse:
+                        <span>{{ $post->user->name }}</span> disse:
                     </strong>
-                    <br>
-                    {{ $post->content }}
-                    <br>
-                    @can('authenticated')
-                        @if (Auth::user()->id === $post->user_id)
-                            <form action="{{ route('post.destroy', $post->id) }}" method="post">
-                                @csrf
-                                @method('delete')
-                                <button>Deletar</button>
-                            </form>
-                        @endif
-                        @if (Auth::user()->permission_id === 2 || Auth::user()->permission_id === 3)
-                            <form action="{{ route('moderator.post.destroy', $post->id) }}" method="post">
-                                @csrf
-                                @method('delete')
-                                <button>Deletar como moderador</button>
-                            </form>
-                        @endif
-                    @endcan
+                </div>
+
+                    <div class="post-content">
+                        {{ $post->content }}
+                    </div>
+
                     <a href="{{ route('post.show', $post->id) }}">Ver mais</a>
-                    <br>
+                    
+                    <div class="posts-buttons">
+                        @can('authenticated')
+                            @if (Auth::user()->id === $post->user_id)
+                                <form action="{{ route('post.destroy', $post->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="delete-storie">Excluir</button>
+                                </form>
+                            @endif
+                            @if (Auth::user()->permission_id === 2 || Auth::user()->permission_id === 3)
+                                <form action="{{ route('moderator.post.destroy', $post->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="delete-storie">Excluir como moderador</button>
+                                </form>
+                            @endif
+                        @endcan
+                    </div>
+                    
                 </p>
+                </div>
             @empty
-                <h2>Ninguém publicou nada ainda. Seja o primeiro!</h2>
+                <h2 class="central-msg">Ninguém publicou nada ainda. Seja o primeiro!</h2>
             @endforelse
-        </div>
     </div>
 @endsection
